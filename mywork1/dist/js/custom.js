a25cd198b409b85d5ce4d92b92089ed41f2dc0fb
Mohamed-magdy29 <45759814+Mohamed-magdy29@users.noreply.github.com>
2019-02-17 05:08:35 +0200

    Add files via upload

---
+++ mywork1/dist/js/custom.js
@@ -0,0 +1,10 @@
+$('.slider-component').slick({
+    rtl: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 500,
+    dots: false,
+    arrows:true,
+    prevArrow: '<a href="javascript:void(0)" class="slick-prev ti-arrow-left-chevron"></a>',
+    nextArrow: '<a href="javascript:void(0)" class="slick-next ti-arrow-right-chevron"></a>',
+});
